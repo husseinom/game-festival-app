35b89964d031443dcf466b88143f079ad340656a
husseinom <o.samy1110@gmail.com>
2025-11-18 10:12:01 +0100

    Clean Frontend to start

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,35 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
-import { Router, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
-import { Login } from './shared/auth/login/login';
-import { AuthService } from './shared/auth/auth-service';
-import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar, MatToolbarRow, RouterOutlet, RouterLinkActive, RouterModule, MatButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
-
-  private authService = inject(AuthService);
-  readonly router = inject(Router);
-
-  readonly loggedIn = this.authService.isLoggedIn;
-  readonly isAdmin = this.authService.isAdmin;
-  readonly currentUser = this.authService.currentUser;
-  readonly isLoading = this.authService.isLoading;
-  
-
-  LoggedOut(): void{
-    setTimeout(() => {
-    this.authService.logout();
-    setTimeout(() => {
-      this.router.navigate(['/login']);
-    }, 50);
-    }, 0);
-
-  }
 }
